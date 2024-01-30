3666ced8de6651a1d827164afb2ce1d9c42aab75
Axel Hallefält <axelhallefalt@protonmail.com>
2024-01-31 00:39:36 +0100

    README improvement

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# StiL-API
-[Link to page](https://03haax.github.io/StiL-API)
+# [StiL - <em>Your electronic key to Lund University</em>](https://03haax.github.io/stiL)
+
+### Description
+This is an API to get the name associated with a Lund University StiL. The database contains about 113000 rows and was last updated 2024-05-11 meaning if you were registered later your entry will not exist.
+
+### Usage
+https://qiwmtbkaewogvhiojlkz.supabase.co/rest/v1/rpc/get?input=XX0000XX-s@student.lu.se
+
+With header **apikey**:
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpd210YmthZXdvZ3ZoaW9qbGt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM3NDEyOTcsImV4cCI6MTk5OTMxNzI5N30.cIGIIUWBhoIdi8R9IkUaVtGbTVZ7Z6fdVQJGBjIkCeA
+
+### Why
+During development of an app we wanted to use StiL to limit users to Lund University and also tie the account to their name. You could then send an email confirm to the account.
+
+### Contact
+If you want to use this API for your own endeavors feel free to do so! If the service is down or you want to me to update the database you can reach me at [axelhallefalt@protonmail.com](mailto:axelhallefalt@protonmail.com)
+
+<em>Please don't sue me lol</em>
--- script.js
+++ script.js
@@ -1,25 +1,31 @@
-const url = 'https://qiwmtbkaewogvhiojlkz.supabase.co/rest/v1/rpc/get?input='
-const apikey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpd210YmthZXdvZ3ZoaW9qbGt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM3NDEyOTcsImV4cCI6MTk5OTMxNzI5N30.cIGIIUWBhoIdi8R9IkUaVtGbTVZ7Z6fdVQJGBjIkCeA'
+const url = "https://qiwmtbkaewogvhiojlkz.supabase.co/rest/v1/rpc/get?input=";
+const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpd210YmthZXdvZ3ZoaW9qbGt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM3NDEyOTcsImV4cCI6MTk5OTMxNzI5N30.cIGIIUWBhoIdi8R9IkUaVtGbTVZ7Z6fdVQJGBjIkCeA";
 
-const form = document.getElementById("form")
-const input = document.getElementById("stilid")
-const output = document.getElementById("output")
+async function getName(stiL) {
+  const mail = stiL.substring(0, 8) + "-s@student.lu.se";
+  const response = await fetch(url + mail, {
+    method: "GET",
+    headers: { apikey },
+  });
 
-form.addEventListener("submit", async e => {
-    e.preventDefault()
+  return response;
+}
 
-    const response = await fetch(url + input.value + '@student.lu.se', {
-        method: "GET",
-        headers: { apikey }
-    });
+const form = document.getElementById("form");
+const input = document.getElementById("stilid");
+const output = document.getElementById("output");
 
-    let data = '';
-    if (response.status !== 200) {
-        data = 'ERROR'
-    } else {
-        data = await response.json()
-        if (!data) data = 'Not found'
-    }
 
-    output.innerHTML = data
-})
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const response = await getName(input.value);
+  let data = "";
+  if (response.status !== 200) {
+    data = "Database error";
+  } else {
+    data = await response.json();
+    if (!data) data = "Not found";
+  }
+
+  output.innerHTML = data;
+});
